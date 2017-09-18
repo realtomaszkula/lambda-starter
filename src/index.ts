5f44c6dfcf31a46dcf54ec5774e2b55fd5328cd4
Tomasz Kula <tomaszkula07@gmail.com>
2017-09-18 16:19:41 +0200

    Add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { Context, APIGatewayEvent, Callback } from 'aws-lambda';
-import { LambdaEnv } from './env';
-import * as AWS from 'aws-sdk';
+import { Context, APIGatewayEvent, Callback } from "aws-lambda";
+import { LambdaEnv } from "./env";
+import * as AWS from "aws-sdk";
 
-export const handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+export const handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback,
+) => {
   const env: LambdaEnv = process.env as any;
   AWS.config.region = env.awsRegion;
 };
